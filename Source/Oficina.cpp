4dfe90b8ec36b7be61f86b405658ec154c83eaf3
xRuiAlves <up201606746@fe.up.pt>
2017-12-20 21:17:11 +0000

    Made changes in Oficina::printCompleteInfo()

--- Source/Oficina.cpp
+++ Source/Oficina.cpp
@@ -36,7 +36,7 @@ void Oficina::printSimplifiedInfo(std::ostream & os) const{
 }
 
 void Oficina::printCompleteInfo() const{
-	std::cout << "***  Oficina #" << id << "  ***" << std::endl;
+	std::cout << "***  Oficina no. " << id << "  ***" << std::endl;
 	std::cout << "Disponibilidade: " << disponibilidade << std::endl;
 	std::cout << "Marcas Representadas:" << std::endl;
 	for (unsigned int i=0 ; i<marcasRepresentadas.size() ; i++){
